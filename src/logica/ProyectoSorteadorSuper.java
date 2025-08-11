5a2b9952b0b81f68aaf90f2f058cd522cd380045
Gastón Climent <gaston.climent@hotmail.com>
2025-08-11 19:07:16 -0300

    sorteoSupermercado
    Finalizacion de Trabajo

--- src/logica/ProyectoSorteadorSuper.java
+++ src/logica/ProyectoSorteadorSuper.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package logica;
 
-/**
- *
- * @author Gaston
- */
+import IGU.Main;
+
 public class ProyectoSorteadorSuper {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Main ventana = new Main();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
     }
     
 }
